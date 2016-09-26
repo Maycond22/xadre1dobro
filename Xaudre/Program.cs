ff61ae1bd86e4c99b32626c78b525db19955cb86
MAYCON DOUGLAS DA SILVA <02081410656@est.udicentro.local>
2016-09-26 09:35:08 -0300

    Mudando de dobro pra triplo

--- Xaudre/Program.cs
+++ Xaudre/Program.cs
@@ -14,9 +14,9 @@ namespace Xaudre
 
             int x = int.Parse(Console.ReadLine());
 
-            int y = 2 * x;
+            int y = 3 * x;
 
-            Console.WriteLine("DOBRO = " + y);
+            Console.WriteLine("TRIPLO = " + y);
 
             Console.ReadLine();
         }
